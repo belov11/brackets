253a8c11fa04b2723d919a8c95006f9fc03e3de4
belov11 <pasha_belov_96@mail.ru>
2022-07-20 09:41:29 +0300

    feat: add code in index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let res = [];
+  let arr = bracketsConfig.reduce((arr, item) => arr.concat(item), []);
+  //console.log(arr);
+  for (let elem of str) {
+    let index = arr.indexOf(elem);
+    //console.log(elem);
+    //console.log(index);
+    if (index % 2 === 0) {
+      if (elem === arr[index + 1] && res.slice(-1).join("") !== elem) {
+        res.push(elem);
+      } else if (elem === arr[index + 1] && res.slice(-1).join("") === elem) {
+        res.pop();
+      }
+      else {
+        res.push(elem);
+      }
+    }
+    else {
+      if (arr[index - 1] !== res.slice(-1).join("")) {
+        return false;
+       } 
+      if (res.length === 0) {
+        return false;
+      } 
+      else {
+        res.pop()
+      }
+    }
+
+    //console.log(res);
+  }
+      
+  
+  //console.log(res.length);
+
+  return res.length === 0;
+
 }
